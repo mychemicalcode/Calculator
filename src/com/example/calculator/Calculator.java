8b4fc03696758500991869c57065fb4fc484039c
mychemicalcode <jty7027@gmail.com>
2025-03-06 10:43:20 +0900

    계산기 Lv.1 주석 삭제 250306 10:42

--- src/com/example/calculator/Calculator.java
+++ src/com/example/calculator/Calculator.java
@@ -14,11 +14,11 @@ public class Calculator {
     int num1 = 0;
     int num2 = 0;
     int result = 0;
-
-    static int sumPlus = num1 + num2;
-    static int sumMinus = num1 - num2;
-    static int sumMul = num1 * num2;
-    static int sumDiv = num1 / num2;
+//
+//    static int sumPlus = num1 + num2;
+//    static int sumMinus = num1 - num2;
+//    static int sumMul = num1 * num2;
+//    static int sumDiv = num1 / num2;
 
 
 
