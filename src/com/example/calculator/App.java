4d39776c0fc2074e08287a67b2df3d58fdada393
mychemicalcode <jty7027@gmail.com>
2025-03-06 10:42:45 +0900

    계산기 Lv.1 주석 삭제 250306 10:42

--- src/com/example/calculator/App.java
+++ src/com/example/calculator/App.java
@@ -33,7 +33,6 @@ public class App {
                 System.out.print("숫자를 입력하셨습니다. 계산을 진행할 연산기호를 하나만 입력하여 주십시오.");
             }
             char ch = sc.next().charAt(0);
-            // 연산 다 때려박기, 공백 연산 때려박기
 
             int result = 0;
 
@@ -51,7 +50,7 @@ public class App {
                 if (num2 == 0) {
                     System.out.println("분모에는 0이 올 수 없습니다. 처음부터 다시 계산해주세요");
                     continue;
-                    // 예외처리 명시된 내용에 맞게 수정
+
                 } else {
                     result = num1 / num2;
                 }
@@ -73,43 +72,3 @@ public class App {
         ;
     }
 }
-
-
-
-//next int or next double
-// 엔터 인식해버리므로 nextLine을 한 번 더 넣어줘야함
-
-// System.out.println("더 계산하시겠습니까? ( Y / N ) ");
-// keep = sc.next().charAt(0);
-// System.out.println(keep);
-// } while (keep == 'Y' || keep == 'y');
-
-
-
-// 변수 : num1, num2, result, charAt,
-
-
-//        System.out.println("입력 값은 " + num1 + " 입니다. 맞습니까?! 맞으면 1 틀리면 2, 처음으로 돌아가려면 #를 눌러주십시오.");
-//        Scanner sc = new Scanner(System.in);
-//        if (sc.next().charAt(0) == '#') {
-//            continue;
-//        }
-//
-//
-//        System.out.println("두번재 숫자를 입력해주세요.");
-//        int num2 = sc.nextInt();
-//
-//        System.out.println("필요한 연산을 입력해주세요. (+, -, *, /");
-//        char charAt = sc.next().charAt(0);
-//
-//        System.out.println("입력 값은 " + num1 + charAt + num2 + " 입니다. 맞습니까?!");
-
-// $ git add.
-// $ git remote set-url origin main 하면 여기에 url 뜸
-// $ git
-//
-//
-// ㅜㅜ_ㅜㅜ
-//
-//git branch -M main
-//git push -u origin main
--- src/com/example/calculator/Calculator.java
+++ src/com/example/calculator/Calculator.java
@@ -1,4 +1,29 @@
 package com.example.calculator;
 
+import java.util.Scanner;
+
 public class Calculator {
+
+
+    String codeExit = "exit";
+    String keep = "";
+    Scanner sc = new Scanner(System.in);
+
+
+    char ch = sc.next().charAt(0);
+    int num1 = 0;
+    int num2 = 0;
+    int result = 0;
+
+    static int sumPlus = num1 + num2;
+    static int sumMinus = num1 - num2;
+    static int sumMul = num1 * num2;
+    static int sumDiv = num1 / num2;
+
+
+
+
+
+
+
 }
